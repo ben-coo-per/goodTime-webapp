209f2848057a3377970a72618aab909f1de9dd02
Ben Cooper <cooperben83@gmail.com>
2022-09-10 12:11:09 -0400

    fixed storybook for loading indicator; closes #36

--- web/src/components/LoadingIndicator/LoadingIndicator.stories.tsx
+++ web/src/components/LoadingIndicator/LoadingIndicator.stories.tsx
@@ -1,7 +1,11 @@
 import LoadingIndicator from './LoadingIndicator'
 
 export const generated = (args) => {
-  return <LoadingIndicator {...args} />
+  return (
+    <div className="grid h-screen place-items-center">
+      <LoadingIndicator {...args} />
+    </div>
+  )
 }
 
 export default { title: 'Components/LoadingIndicator' }
