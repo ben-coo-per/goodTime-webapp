dfeb88b65cafd84947392dddf3eb9b9e226e3eed
Ben Cooper <cooperben83@gmail.com>
2022-09-21 11:13:03 -0400

    fixed broken storybook components

--- web/src/components/EventResponseForm/EventResponseForm.stories.tsx
+++ web/src/components/EventResponseForm/EventResponseForm.stories.tsx
@@ -1,7 +1,30 @@
-import EventResponseForm from './EventResponseForm'
+import EventResponseForm, { ProvidedTimes } from './EventResponseForm'
+
+const times: ProvidedTimes[] = [
+  {
+    id: 40,
+    startTime: 1662786000,
+    endTime: 1662800400,
+    user: {
+      id: 'cl7tgkzqk0027jk0qawvev43f',
+      displayName: 'Beb',
+      phoneNumber: '7132546843',
+    },
+  },
+  {
+    id: 41,
+    startTime: 1662879600,
+    endTime: 1662886800,
+    user: {
+      id: 'cl7tgkzqk0027jk0qawvev43f',
+      displayName: 'Beb',
+      phoneNumber: '7132546843',
+    },
+  },
+]
 
 export const generated = () => {
-  return <EventResponseForm />
+  return <EventResponseForm times={times} />
 }
 
 export default { title: 'Components/EventResponseForm' }
--- web/src/components/EventResponseReview/EventResponseReview.stories.tsx
+++ web/src/components/EventResponseReview/EventResponseReview.stories.tsx
@@ -1,7 +1,45 @@
+import { ProvidedTimes } from '../EventResponseForm/EventResponseForm'
+
 import EventResponseReview from './EventResponseReview'
 
+const selectedTimes: ProvidedTimes[] = [
+  {
+    id: 41,
+    startTime: 2662879600,
+    endTime: 2662886800,
+    user: {
+      id: 'cl7tgkzqk0027jk0qawvev43f',
+      displayName: 'Beb',
+      phoneNumber: '7132546843',
+    },
+  },
+]
+
+const baseTimes: ProvidedTimes[] = [
+  {
+    id: 40,
+    startTime: 2662786000,
+    endTime: 2662800400,
+    user: {
+      id: 'cl7tgkzqk0027jk0qawvev43f',
+      displayName: 'Beb',
+      phoneNumber: '7132546843',
+    },
+  },
+  {
+    id: 41,
+    startTime: 2662879600,
+    endTime: 2662886800,
+    user: {
+      id: 'cl7tgkzqk0027jk0qawvev43f',
+      displayName: 'Beb',
+      phoneNumber: '7132546843',
+    },
+  },
+]
+
 export const generated = () => {
-  return <EventResponseReview />
+  return <EventResponseReview times={baseTimes} selectedTimes={selectedTimes} />
 }
 
-export default { title: 'Components/EventResponseSummary' }
+export default { title: 'Components/EventResponseReview' }
